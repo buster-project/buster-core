90d18fa60b0e3f155e43036033d4b07f44617565
narustudio <slackjawed12@gmail.com>
2023-05-27 16:46:41 +0900

    first commit

---
+++ src/main/java/com/slackjawed/buster/BusterApplication.java
@@ -0,0 +1,13 @@
+package com.slackjawed.buster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusterApplication.class, args);
+    }
+
+}
